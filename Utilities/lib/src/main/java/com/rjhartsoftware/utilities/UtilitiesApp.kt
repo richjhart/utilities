2999e0ca915d3fb07c531f9bd32d63665ada215e
Richard Hart <richard@rjhartsoftware.co.uk>
2022-04-16 15:59:49 +0100

    Fix minor issues

--- Utilities/lib/src/main/java/com/rjhartsoftware/utilities/UtilitiesApp.kt
+++ Utilities/lib/src/main/java/com/rjhartsoftware/utilities/UtilitiesApp.kt
@@ -1,10 +1,9 @@
 package com.rjhartsoftware.utilities
 
-import android.app.Application
 import androidx.multidex.MultiDexApplication
 import com.rjhartsoftware.utilities.D.init
 
-class UtilitiesApp : MultiDexApplication() {
+open class UtilitiesApp : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         init(this)
